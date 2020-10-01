e76798fecb9b0ce30ab6fc01e69bca1f1118bb6e
Dave Llewellyn <439837@gmail.com>
2020-10-01 12:24:01 +0100

    added cli skeleton code

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/ONSdigital/dp-document-db
 
 go 1.13
+
+require (
+	github.com/ONSdigital/log.go v1.0.1
+	github.com/spf13/cobra v1.0.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/ONSdigital/log.go/log"
+	"github.com/spf13/cobra"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	if err := run(); err != nil {
+		log.Event(context.Background(), "application error", log.ERROR, log.Error(err))
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	root := &cobra.Command{
+		Use:   "docdb",
+		Short: "TODO",
+	}
+
+	root.AddCommand(ping())
+
+	return root.Execute()
+}
+
+func ping() *cobra.Command {
+	return &cobra.Command{
+		Use:   "docdb",
+		Short: "TODO",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Println("Hello world!")
+			return nil
+		},
+	}
 }
